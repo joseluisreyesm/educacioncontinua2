cae076af9004ca33d7b71b9dae2cd65a032a55bb
joseluisreyesm <48143883+joseluisreyesm@users.noreply.github.com>
2021-06-13 20:03:02 -0500

    Se corrigio la vista de register.html, se modifico la nav bar base.

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -3,23 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class UsuarioForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].label = 'Introduce tu contraseña'
+        self.fields['password2'].label = 'Introduce tu contraseña nuevamente'
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+        self.fields['username'].help_text = ''
+
     class Meta:
         model = User
-
         fields = ['username', 'first_name', 'last_name', 'email']
-
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'}),
             'username': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Matrícula'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Correo electrónico'}),
-
         }
-
-        labels = {
-            'username': 'Matrícula',
-            'first_name': 'Nombre',
-            'last_name': 'Apellidos',
-            'email': 'Correo',
-        }
-
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as gettext
-
-
-# Create your models here.
 
 class Usuario(models.Model):
     matricula = models.IntegerField()
